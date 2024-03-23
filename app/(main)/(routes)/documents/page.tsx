133a79485f16cd61680552c454b7a0a519ef3720
Vaibhav Sanket <101280631+sanky-7@users.noreply.github.com>
2024-03-23 21:27:32 +0530

    daily progress

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
+
+    const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note.",
+        })
+    }
+
   return (
-    <div>DocumentsPage</div>
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+        <Image src="/empty.png" alt="empty" height="400" width="400" className="dark:hidden" />
+        <Image src="/empty-dark.png" alt="empty" height="400" width="400" className="hidden dark:block" />
+        <h2 className="text-xl font-bold">
+            Welcome to {user?.firstName}&apos;s DotNote
+        </h2>
+        <Button onClick={onCreate}>
+            <PlusCircle className="h-4 w-4 mr-2" />
+            Create a note
+        </Button>
+    </div>
   )
 }
 
---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  action: string;
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal = ({ action, children, onConfirm }: ConfirmModalProps) => {
+  const handleConfirm = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e) => e.stopPropagation()}>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          This action cannot be undone.
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>{action}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
