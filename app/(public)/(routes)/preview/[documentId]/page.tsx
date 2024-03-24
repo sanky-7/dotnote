3d46ea168ff4740d35d965bf0a248fdaa46b2bf1
Vaibhav Sanket <101280631+sanky-7@users.noreply.github.com>
2024-03-24 07:35:21 +0530

    more adjustment

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -9,6 +9,7 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
 import { ModeToggle } from "@/components/mode-toggle";
+import Head from "next/head";
 
 interface DocumentIdPageProps {
   params: {
@@ -40,17 +41,24 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   if (document === undefined) {
     return (
-      <div>
-        <Cover.Skeleton />
-        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
-          <div className="space-y-4 pl-8 pt-4">
-            <Skeleton className="h-14 w-[50%]" />
-            <Skeleton className="h-4 w-[80%]" />
-            <Skeleton className="h-4 w-[40%]" />
-            <Skeleton className="h-4 w-[60%]" />
+      <>
+      <Head>
+        <title>{document!.title}</title>
+        <meta property="og:title" content={document!.title} />
+        <meta property="og:image" content={document!.coverImage} />
+      </Head>
+        <div>
+          <Cover.Skeleton />
+          <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+            <div className="space-y-4 pl-8 pt-4">
+              <Skeleton className="h-14 w-[50%]" />
+              <Skeleton className="h-4 w-[80%]" />
+              <Skeleton className="h-4 w-[40%]" />
+              <Skeleton className="h-4 w-[60%]" />
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 
@@ -60,13 +68,17 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40 dark:bg-[#1F1F1F]">
-        <div className="absolute top-5 right-5 z-[99999]">
-            <ModeToggle />
-        </div>
+      <div className="absolute top-5 right-5 z-[99999]">
+        <ModeToggle />
+      </div>
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar preview initialData={document} />
-        <Editor editable={false} onChange={onChange} initialContent={document.content} />
+        <Editor
+          editable={false}
+          onChange={onChange}
+          initialContent={document.content}
+        />
       </div>
     </div>
   );
